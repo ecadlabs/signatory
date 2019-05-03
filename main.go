4f32d0f9266c1531ee65cb3bfefa141d91a4ad01
Simon Boissonneault-Robert <simrob7000@gmail.com>
2019-05-03 09:20:37 -0400

    Add basic config flag

--- main.go
+++ main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/ecadlabs/signatory/config"
 	"github.com/ecadlabs/signatory/server"
 	"github.com/ecadlabs/signatory/signatory"
 	"github.com/ecadlabs/signatory/vault"
+
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
+	log.SetLevel(log.DebugLevel)
+
+	var configFile string
+	flag.StringVar(&configFile, "config", "signatory.yaml", "Config file path")
+
+	flag.Parse()
+
 	c := &config.Config{}
-	c.Read("config.yaml")
+	c.Read(configFile)
+
 	vault := vault.NewAzureVault(&c.Azure, nil)
 	signatory := signatory.NewSignatory([]signatory.Vault{vault})
 
