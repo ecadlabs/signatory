f9473b8f1c747ead36c34e608895f0cc568ea61c
Simon B.Robert <simrob7000@gmail.com>
2019-05-07 16:58:12 -0400

    Add kind to siging metric (#15)
    * Add operation kind to signing metrics

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -10,7 +10,7 @@ import (
 var signingOpCount = prometheus.NewCounterVec(prometheus.CounterOpts{
 	Name: "signing_ops_total",
 	Help: "Total number of signing operations completed.",
-}, []string{"address", "vault", "algorithm"})
+}, []string{"address", "vault", "algorithm", "kind"})
 
 // RegisterHandler register metrics handler
 func RegisterHandler() {
@@ -19,6 +19,6 @@ func RegisterHandler() {
 }
 
 // IncNewSigningOp register a new signing operation with vault
-func IncNewSigningOp(address string, vault string, algorithm string) {
-	signingOpCount.WithLabelValues(address, vault, algorithm).Inc()
+func IncNewSigningOp(address string, vault string, algorithm string, kind string) {
+  signingOpCount.WithLabelValues(address, vault, algorithm, kind).Inc()
 }
