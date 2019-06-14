eb87322e7196afdd4bf4ca40d8f13bd265db3dab
Simon Boissonneault-Robert <simrob7000@gmail.com>
2019-06-14 14:20:59 -0400

    Add key import utilities

--- signatory/signatory.go
+++ signatory/signatory.go
@@ -1,7 +1,6 @@
 package signatory
 
 import (
-	"encoding/base64"
 	"encoding/hex"
 	"fmt"
 	"math/big"
@@ -19,15 +18,6 @@ var (
 	ErrNotSafeToSign = fmt.Errorf("Not safe to sign")
 )
 
-// JWK struct containing a standard key format
-type JWK struct {
-	KeyType string `json:"kty"`
-	D       string `json:"d"`
-	X       string `json:"x"`
-	Y       string `json:"y"`
-	Curve   string `json:"crv"`
-}
-
 type NotifySigning func(address string, vault string, kind string)
 
 // PublicKey alias for an array of byte
@@ -39,12 +29,6 @@ type StoredKey interface {
 	ID() string
 }
 
-// ImportedKey struct containing information about an imported key
-type ImportedKey struct {
-	Hash  string
-	KeyID string
-}
-
 // Vault interface that represent a secure key store
 type Vault interface {
 	GetPublicKey(keyID string) (StoredKey, error)
@@ -52,14 +36,6 @@ type Vault interface {
 	Sign(digest []byte, key StoredKey) ([]byte, error)
 	Import(jwk *JWK) (string, error)
 	Name() string
-}
-
-// KeyPair interface that represent an elliptic curve key pair
-type KeyPair interface {
-	X() []byte
-	Y() []byte
-	D() []byte
-	CurveName() string
 }
 
 // Watermark interface for service that allow double bake check
@@ -231,50 +207,4 @@ func (s *Signatory) GetPublicKey(keyHash string) (string, error) {
 		return "", err
 	}
 	return tezos.EncodePubKey(keyHash, pubKey.PublicKey()), nil
-}
-
-// Import a keyPair inside the vault
-func (s *Signatory) Import(pubkey string, secretKey string, vault Vault) (*ImportedKey, error) {
-	keyPair := tezos.NewKeyPair(pubkey, secretKey)
-	err := keyPair.Validate()
-
-	if err != nil {
-		return nil, err
-	}
-
-	jwk, err := s.ToJWK(keyPair)
-
-	if err != nil {
-		return nil, err
-	}
-
-	keyID, err := vault.Import(jwk)
-
-	if err != nil {
-		return nil, err
-	}
-
-	hash, err := keyPair.PubKeyHash()
-
-	if err != nil {
-		return nil, err
-	}
-
-	importedKey := &ImportedKey{
-		KeyID: keyID,
-		Hash:  hash,
-	}
-
-	return importedKey, nil
-}
-
-// ToJWK Convert a keyPair to a JWK
-func (s *Signatory) ToJWK(k KeyPair) (*JWK, error) {
-	return &JWK{
-		X:       base64.StdEncoding.EncodeToString(k.X()),
-		Y:       base64.StdEncoding.EncodeToString(k.Y()),
-		D:       base64.StdEncoding.EncodeToString(k.D()),
-		KeyType: "EC",
-		Curve:   k.CurveName(),
-	}, nil
 }
