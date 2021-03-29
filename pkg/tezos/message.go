86ec5f396c09417f76592f0d631f81faa13b841e
amurugan <abinesh.murgan@ibm.com>
2021-03-29 14:12:05 +0530

    reduced slice size

--- pkg/tezos/message.go
+++ pkg/tezos/message.go
@@ -72,11 +72,12 @@ func (u *UnsignedOperation) OperationKinds() OpsAmount {
 	}
 	ops.Amount = "amount_total=" + totamount.String()
 	i := 0
-	ops.Ops = make([]string, 11)
+	ops.Ops = make([]string, 2)
 	for k, v := range kindCount {
 		ops.Ops[i] = k + "=" + strconv.Itoa(v)
 		i++
 	}
+	fmt.Println(ops)
 	return ops
 }
 
