bb705f04f79c72733e6b6793c28043f4a4aac14a
AbineshECAD <abinesh@ecadlabs.com>
2022-03-18 19:15:33 +0530

    Removed TODO which is done inprevious commit

--- pkg/vault/aws/awskms.go
+++ pkg/vault/aws/awskms.go
@@ -114,7 +114,6 @@ func (c *awsKMSIterator) Next() (key vault.StoredKey, err error) {
 }
 
 // ListPublicKeys returns a list of keys stored under the backend
-// TODO: Handle errors in anyother key in the vault
 func (c *Vault) ListPublicKeys(ctx context.Context) vault.StoredKeysIterator {
 
 	var lkout *kms.ListKeysOutput
