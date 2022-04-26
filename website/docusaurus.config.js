7d463df8277f8395cb8ea70ab5911560c8ef28bd
Hamid Goudarzi <51421954+HamidGoudarzi1988@users.noreply.github.com>
2022-04-25 19:14:50 -0700

    footer

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,97 +1,128 @@
 module.exports = {
-  title: 'Signatory - A Tezos Remote Signer',
-  tagline: 'Signatory signs your Tezos Ops while protecting your private keys',
-  url: 'https://signatory.io',
-  baseUrl: '/',
-  favicon: 'img/favicon.ico',
-  organizationName: 'ecadlabs', // Usually your GitHub org/user name.
-  projectName: 'signatory', // Usually your repo name.
-  onBrokenLinks: 'warn',
-  themeConfig: {
-    navbar: {
-      title: 'Signatory Remote Signer',
-      logo: {
-        alt: 'Signatory Signer for Tezos',
-        src: 'img/logo_nib.png',
-      },
-      items: [
-        {
-          to: 'docs/start',
-          activeBasePath: 'docs',
-          label: 'Docs',
-          position: 'left',
-        },
-        {
-          href: 'https://github.com/ecadlabs/signatory',
-          label: 'GitHub',
-          position: 'right',
-        },
-      ],
-    },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Getting Started ',
-              to: 'docs/start',
-            },
-            {
-              label: 'Local Secret',
-              to: 'docs/file_based',
-            },
-            {
-              label: 'YubiHSM',
-              to: 'docs/yubihsm',
-            },
-            {
-              label: 'Azure KMS',
-              to: 'docs/azure_kms',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Tezos Stack Overflow',
-              href: 'https://tezos.stackexchange.com/questions/tagged/remote-signer',
-            },
-          ],
-        },
-        {
-          title: 'Social',
-          items: [
-            {
-              label: 'GitHub',
-              href: 'https://github.com/ecadlabs/signatory',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/signatoryio',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} ECAD Labs Inc.`,
-    },
-  },
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      {
-        docs: {
-          path: '../docs',
-          sidebarPath: require.resolve('./sidebars.js'),
-          editUrl:
-            'https://github.com/ecadlabs/signatory/edit/master/website/',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      },
-    ],
-  ],
+	title: 'Signatory - A Tezos Remote Signer',
+	tagline: 'Signatory signs your Tezos Ops while protecting your private keys',
+	url: 'https://signatory.io',
+	baseUrl: '/',
+	favicon: 'img/favicon.ico',
+	organizationName: 'ecadlabs', // Usually your GitHub org/user name.
+	projectName: 'signatory', // Usually your repo name.
+	onBrokenLinks: 'warn',
+	themeConfig: {
+		navbar: {
+			title: 'Signatory Remote Signer',
+			logo: {
+				alt: 'Signatory Signer for Tezos',
+				src: 'img/logo_nib.png',
+			},
+			items: [
+				{
+					to: 'docs/start',
+					activeBasePath: 'docs',
+					label: 'Docs',
+					position: 'left',
+				},
+				{
+					href: 'https://github.com/ecadlabs/signatory',
+					label: 'GitHub',
+					position: 'right',
+				},
+			],
+		},
+		footer: {
+			links: [
+				{
+					title: 'Contact',
+					items: [
+						{
+							label: 'Report Issues',
+							to: 'https://github.com/ecadlabs/taqueria/issues/new/choose',
+						},
+						{
+							label: 'Contribute',
+							to: 'https://github.com/ecadlabs/taquito/blob/master/CONTRIBUTING.md',
+						},
+					],
+				},
+
+				{
+					title: 'Community',
+					items: [
+						{
+							label: 'Stack Exchange',
+							to: 'https://tezos.stackexchange.com/questions/tagged/taqueria',
+						},
+						{
+							label: 'Discord',
+							to: 'https://discord.gg/bujt7syVVT',
+						},
+						{
+							label: 'Twitter',
+							to: 'https://twitter.com/tezostaqueria',
+						},
+						{
+							label: 'Code of Conduct',
+							to: 'https://github.com/ecadlabs/taquito/blob/master/code-of-conduct.md',
+						},
+						{
+							label: 'GitHub',
+							to: 'https://github.com/ecadlabs/taqueria',
+						},
+					],
+				},
+				{
+					title: 'Docs',
+					items: [
+						{
+							label: 'Quick Start',
+							to: '/docs/quick_start',
+						},
+						{
+							label: 'TypeDoc Reference',
+							to: 'https://tezostaquito.io/typedoc',
+						},
+					],
+				},
+				{
+					items: [
+						{
+							html: `image`,
+						},
+						{
+							html: `
+									<p class='footerDescription'>
+										Faplaren krorar whataboutism. Krorat kroligen. 
+									</p>
+								  `,
+						},
+						{
+							html: `
+									<a class='footerButton' href='https://github.com/ecadlabs/taqueria'>
+										GITHUB
+									</a>
+								  `,
+						},
+						{
+							html: `form`,
+						},
+					],
+				},
+			],
+		},
+	},
+	presets: [
+		[
+			'@docusaurus/preset-classic',
+			{
+				docs: {
+					path: '../docs',
+					sidebarPath: require.resolve('./sidebars.js'),
+					editUrl: 'https://github.com/ecadlabs/signatory/edit/master/website/',
+				},
+				theme: {
+					customCss: require.resolve('./src/css/custom.scss'),
+				},
+			},
+		],
+	],
+	plugins: ['docusaurus-plugin-sass'],
 };
--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -5,90 +5,99 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import styles from './styles.module.css';
+import FooterTop from '../components/FooterTop/FooterTop';
 
-const features = [
-  {
-    title: <>Easy to start, trivial to secure</>,
-    imageUrl: 'img/undraw_security_o890.svg',
-    description: (
-      <>
-        Start prototyping your infrastructure with an on-disk key, switch to an HSM when it makes sense
-      </>
-    ),
-  },
-  {
-    title: <>Signatory signs only the operations you want</>,
-    imageUrl: 'img/undraw_data_processing_yrrv.svg',
-    description: (
-      <>
-        Set policy on the type of Tezos Operations you want to allow signing. Are you running a baker? Limit it to blocks and endorsements. Institution? Signatory can enforce policies.
-      </>
-    ),
-  },
-  {
-    title: <>Built with observability</>,
-    imageUrl: 'img/undraw_observations_mejb.svg',
-    description: (
-      <>
-        Critical infrastructure monitoring is crucial. Signatory exposes operational metrics for Prometheus allowing teams to monitor operations with the tools they have already invested in
-      </>
-    ),
-  },
-];
+// const features = [
+// 	{
+// 		title: <>Easy to start, trivial to secure</>,
+// 		imageUrl: 'img/undraw_security_o890.svg',
+// 		description: (
+// 			<>
+// 				Start prototyping your infrastructure with an on-disk key, switch to an
+// 				HSM when it makes sense
+// 			</>
+// 		),
+// 	},
+// 	{
+// 		title: <>Signatory signs only the operations you want</>,
+// 		imageUrl: 'img/undraw_data_processing_yrrv.svg',
+// 		description: (
+// 			<>
+// 				Set policy on the type of Tezos Operations you want to allow signing.
+// 				Are you running a baker? Limit it to blocks and endorsements.
+// 				Institution? Signatory can enforce policies.
+// 			</>
+// 		),
+// 	},
+// 	{
+// 		title: <>Built with observability</>,
+// 		imageUrl: 'img/undraw_observations_mejb.svg',
+// 		description: (
+// 			<>
+// 				Critical infrastructure monitoring is crucial. Signatory exposes
+// 				operational metrics for Prometheus allowing teams to monitor operations
+// 				with the tools they have already invested in
+// 			</>
+// 		),
+// 	},
+// ];
 
-function Feature({imageUrl, title, description}) {
-  const imgUrl = useBaseUrl(imageUrl);
-  return (
-    <div className={classnames('col col--4', styles.feature)}>
-      {imgUrl && (
-        <div className="text--center">
-          <img className={styles.featureImage} src={imgUrl} alt={title} />
-        </div>
-      )}
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </div>
-  );
-}
+// function Feature({ imageUrl, title, description }) {
+// 	const imgUrl = useBaseUrl(imageUrl);
+// 	return (
+// 		<div className={classnames('col col--4', styles.feature)}>
+// 			{imgUrl && (
+// 				<div className='text--center'>
+// 					<img className={styles.featureImage} src={imgUrl} alt={title} />
+// 				</div>
+// 			)}
+// 			<h3>{title}</h3>
+// 			<p>{description}</p>
+// 		</div>
+// 	);
+// }
 
 function Home() {
-  const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
-  return (
-    <Layout
-      title={`${siteConfig.title}`}
-      description="A Remote Signer for Tezos that keeps your keys in an HSM (Yubi, Azure KMS, Google KMS)<head />">
-      <header className={classnames('hero hero--primary', styles.heroBanner)}>
-        <div className="container">
-          <h1 className="hero__title">{siteConfig.title}</h1>
-          <p className="hero__subtitle">{siteConfig.tagline}</p>
-          <div className={styles.buttons}>
-            <Link
-              className={classnames(
-                'button button--outline button--secondary button--lg',
-                styles.getStarted,
-              )}
-              to={useBaseUrl('docs/start')}>
-              Get Started
-            </Link>
-          </div>
-        </div>
-      </header>
-      <main>
-        {features && features.length && (
-          <section className={styles.features}>
-            <div className="container">
-              <div className="row">
-                {features.map((props, idx) => (
-                  <Feature key={idx} {...props} />
-                ))}
-              </div>
-            </div>
-          </section>
-        )}
-      </main>
-    </Layout>
-  );
+	const context = useDocusaurusContext();
+	const { siteConfig = {} } = context;
+	return (
+		<Layout
+			title={`${siteConfig.title}`}
+			description='A Remote Signer for Tezos that keeps your keys in an HSM (Yubi, Azure KMS, Google KMS)<head />'
+		>
+			{/* <header className={classnames('hero hero--primary', styles.heroBanner)}>
+				<div className='container'>
+					<h1 className='hero__title'>{siteConfig.title}</h1>
+					<p className='hero__subtitle'>{siteConfig.tagline}</p>
+					<div className={styles.buttons}>
+						<Link
+							className={classnames(
+								'button button--outline button--secondary button--lg',
+								styles.getStarted
+							)}
+							to={useBaseUrl('docs/start')}
+						>
+							Get Started
+						</Link>
+					</div>
+				</div>
+			</header> */}
+			<main>
+				{/* {features && features.length && (
+					<section className={styles.features}>
+						<div className='container'>
+							<div className='row'>
+								{features.map((props, idx) => (
+									<Feature key={idx} {...props} />
+								))}
+							</div>
+						</div>
+					</section>
+				)} */}
+				<FooterTop />
+			</main>
+		</Layout>
+	);
 }
 
 export default Home;
