83be99f00f7a7c317a97d66bfd4db9bc48c809c8
AbineshECAD <82827388+AbineshECAD@users.noreply.github.com>
2022-09-09 15:38:28 +0530

    Test case for infinite loop when vault errors (#219)
    * Test case for infinite loop when vault errors
    
    * Initialized all generic functions
    
    * Corrected variable names
    
    * 2 more test cases for ListPublicKeys

---
+++ pkg/signatory/utils_test.go
@@ -0,0 +1,53 @@
+package signatory_test
+
+import (
+	"context"
+
+	"github.com/ecadlabs/signatory/pkg/cryptoutils"
+	"github.com/ecadlabs/signatory/pkg/vault"
+)
+
+type GetPublicKey func(ctx context.Context, id string) (vault.StoredKey, error)
+type ListPublicKeys func(ctx context.Context) vault.StoredKeysIterator
+type Sign func(ctx context.Context, digest []byte, key vault.StoredKey) (cryptoutils.Signature, error)
+type Name func() string
+type Next func() (vault.StoredKey, error)
+
+type TestVault struct {
+	vaultname string
+	gp        GetPublicKey
+	lp        ListPublicKeys
+	si        Sign
+	na        Name
+}
+
+func NewTestVault(g GetPublicKey, l ListPublicKeys, s Sign, n Name, vn string) *TestVault {
+
+	return &TestVault{
+		vaultname: vn,
+		gp:        g,
+		lp:        l,
+		si:        s,
+		na:        n,
+	}
+}
+
+type TestKeyIterator struct {
+	idx int
+	nxt func(idx int) (key vault.StoredKey, err error)
+}
+
+func (it *TestKeyIterator) Next() (key vault.StoredKey, err error) {
+	it.idx += 1
+	return it.nxt(it.idx - 1)
+}
+func (v *TestVault) GetPublicKey(ctx context.Context, id string) (vault.StoredKey, error) {
+	return v.gp(ctx, id)
+}
+func (v *TestVault) ListPublicKeys(ctx context.Context) vault.StoredKeysIterator {
+	return v.lp(ctx)
+}
+func (v *TestVault) Sign(ctx context.Context, digest []byte, key vault.StoredKey) (cryptoutils.Signature, error) {
+	return v.si(ctx, digest, key)
+}
+func (v *TestVault) Name() string { return v.na() }
