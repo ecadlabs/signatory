2450a00f6216b80796b29bf2930ebd61cf48f70d
Eugene Zagidullin <e.asphyx@gmail.com>
2022-09-25 19:57:59 +0300

    more tests

--- pkg/signatory/policy_hook_test.go
+++ pkg/signatory/policy_hook_test.go
@@ -23,7 +23,7 @@ import (
 	yaml "gopkg.in/yaml.v3"
 )
 
-func serveHook(status int, pk cryptoutils.PrivateKey) (func(w http.ResponseWriter, r *http.Request), error) {
+func serveHookAuth(status int, pk cryptoutils.PrivateKey) (func(w http.ResponseWriter, r *http.Request), error) {
 	pub := pk.Public()
 	hash, err := tezos.EncodePublicKeyHash(pub)
 	if err != nil {
@@ -71,13 +71,19 @@ func serveHook(status int, pk cryptoutils.PrivateKey) (func(w http.ResponseWrite
 	}, nil
 }
 
-func testPolicyHook(t *testing.T, status int) error {
+func serveHook(status int) (func(w http.ResponseWriter, r *http.Request), error) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(status)
+	}, nil
+}
+
+func testPolicyHookAuth(t *testing.T, status int) error {
 	// generate hook authentication key
 	_, hookPk, err := ed25519.GenerateKey(rand.Reader)
 	require.NoError(t, err)
 	hookPub := hookPk.Public()
 
-	handler, err := serveHook(status, hookPk)
+	handler, err := serveHookAuth(status, hookPk)
 	require.NoError(t, err)
 
 	testSrv := httptest.NewServer(http.HandlerFunc(handler))
@@ -117,6 +123,55 @@ func testPolicyHook(t *testing.T, status int) error {
 	msg := mustHex("019caecab9000753d3029bc7d9a36b60cce68ade985a0a16929587166e0d3de61efff2fa31b7116bf670000000005ee3c23b04519d71c4e54089c56773c44979b3ba3d61078ade40332ad81577ae074f653e0e0000001100000001010000000800000000000753d2da051ba81185783e4cbc633cf2ba809139ef07c3e5f6c5867f930e7667b224430000cde7fbbb948e030000")
 	_, err = s.Sign(context.Background(), &signatory.SignRequest{PublicKeyHash: signKeyHash, Message: msg})
 	return err
+}
+
+func testPolicyHook(t *testing.T, status int) error {
+	// generate hook authentication key
+	handler, err := serveHook(status)
+	require.NoError(t, err)
+
+	testSrv := httptest.NewServer(http.HandlerFunc(handler))
+	defer testSrv.Close()
+
+	_, signPk, err := ed25519.GenerateKey(rand.Reader)
+	require.NoError(t, err)
+	signKeyHash, err := tezos.EncodePublicKeyHash(signPk.Public())
+	require.NoError(t, err)
+
+	conf := signatory.Config{
+		Vaults:    map[string]*config.VaultConfig{"mock": {Driver: "mock"}},
+		Watermark: signatory.IgnoreWatermark{},
+		VaultFactory: vault.FactoryFunc(func(ctx context.Context, name string, conf *yaml.Node) (vault.Vault, error) {
+			return memory.New([]*memory.PrivateKey{
+				{
+					PrivateKey: signPk,
+					KeyID:      signKeyHash,
+				},
+			}, "Mock")
+		}),
+		Policy: map[string]*signatory.Policy{
+			signKeyHash: nil,
+		},
+		PolicyHook: &signatory.PolicyHook{
+			Address: testSrv.URL,
+		},
+	}
+
+	s, err := signatory.New(context.Background(), &conf)
+	require.NoError(t, err)
+
+	msg := mustHex("019caecab9000753d3029bc7d9a36b60cce68ade985a0a16929587166e0d3de61efff2fa31b7116bf670000000005ee3c23b04519d71c4e54089c56773c44979b3ba3d61078ade40332ad81577ae074f653e0e0000001100000001010000000800000000000753d2da051ba81185783e4cbc633cf2ba809139ef07c3e5f6c5867f930e7667b224430000cde7fbbb948e030000")
+	_, err = s.Sign(context.Background(), &signatory.SignRequest{PublicKeyHash: signKeyHash, Message: msg})
+	return err
+}
+
+func TestPolicyHookAuth(t *testing.T) {
+	t.Run("Ok", func(t *testing.T) {
+		require.NoError(t, testPolicyHookAuth(t, http.StatusOK))
+	})
+	t.Run("Deny", func(t *testing.T) {
+		require.EqualError(t, testPolicyHookAuth(t, http.StatusForbidden), "policy hook: 403 Forbidden")
+	})
 }
 
 func TestPolicyHook(t *testing.T) {
