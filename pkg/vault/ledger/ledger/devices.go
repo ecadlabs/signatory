52519e1ae147332821bfdcd0dddae9dad676b907
Eugene Zagidullin <e.asphyx@gmail.com>
2022-12-14 22:35:20 +0200

    Ledger/Speculos TCP transport

--- pkg/vault/ledger/ledger/devices.go
+++ pkg/vault/ledger/ledger/devices.go
@@ -20,43 +20,55 @@ type LedgerDeviceInfo struct {
 	LegacyUSBProductID uint16
 	USBOnly            bool
 	MemorySize         int
-	BlockSize          int
-	BluetoothSpec      []*BluetoothSpec
 }
 
-var ledgerDevices = []*LedgerDeviceInfo{
-	&LedgerDeviceInfo{
+var (
+	ledgerBlue = LedgerDeviceInfo{
 		ID:                 "blue",
 		ProductName:        "Ledger Blue",
 		ProductIDMM:        0x00,
 		LegacyUSBProductID: 0x0000,
 		USBOnly:            true,
 		MemorySize:         480 * 1024,
-		BlockSize:          4 * 1024,
-	},
-	&LedgerDeviceInfo{
+	}
+	ledgerNanoS = LedgerDeviceInfo{
 		ID:                 "nanoS",
 		ProductName:        "Ledger Nano S",
 		ProductIDMM:        0x10,
 		LegacyUSBProductID: 0x0001,
 		USBOnly:            true,
 		MemorySize:         320 * 1024,
-		BlockSize:          4 * 1024,
-	},
-	&LedgerDeviceInfo{
+	}
+	ledgerNanoSP = LedgerDeviceInfo{
+		ID:                 "nanoSP",
+		ProductName:        "Ledger Nano S Plus",
+		ProductIDMM:        0x50,
+		LegacyUSBProductID: 0x0005,
+		USBOnly:            true,
+		MemorySize:         1536 * 1024,
+	}
+	ledgerNanoX = LedgerDeviceInfo{
 		ID:                 "nanoX",
 		ProductName:        "Ledger Nano X",
 		ProductIDMM:        0x40,
 		LegacyUSBProductID: 0x0004,
 		USBOnly:            false,
 		MemorySize:         2 * 1024 * 1024,
-		BlockSize:          4 * 1024,
-		BluetoothSpec: []*BluetoothSpec{
-			{
-				ServiceUUID: uuid.MustParse("13d63400-2c97-0004-0000-4c6564676572"),
-				NotifyUUID:  uuid.MustParse("13d63400-2c97-0004-0001-4c6564676572"),
-				WriteUUID:   uuid.MustParse("13d63400-2c97-0004-0002-4c6564676572"),
-			},
-		},
-	},
+	}
+	ledgerNanoFTS = LedgerDeviceInfo{
+		ID:                 "nanoFTS",
+		ProductName:        "Ledger Nano FTS",
+		ProductIDMM:        0x60,
+		LegacyUSBProductID: 0x0006,
+		USBOnly:            false,
+		MemorySize:         1536 * 1024,
+	}
+)
+
+var ledgerDevices = []*LedgerDeviceInfo{
+	&ledgerBlue,
+	&ledgerNanoS,
+	&ledgerNanoSP,
+	&ledgerNanoX,
+	&ledgerNanoFTS,
 }
