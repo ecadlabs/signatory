0e26e5c213eea6247affa2f143e81dde8f2c147c
AbineshECAD <abinesh@ecadlabs.com>
2023-03-08 18:54:34 +0530

    Error test

--- pkg/cryptoutils/pkcs_test.go
+++ pkg/cryptoutils/pkcs_test.go
@@ -7,6 +7,7 @@ import (
 	"crypto/elliptic"
 	"crypto/rand"
 	"crypto/x509"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -34,5 +35,17 @@ func TestParsePublikKey(t *testing.T) {
 	ppk, err := ParsePKIXPublicKey(standard)
 	require.NoError(t, err)
 
-	require.Equal(t, pk, ppk)
+	require.Equal(t, &pk.PublicKey, ppk)
+}
+
+func TestParsePublikKeyTrialingData(t *testing.T) {
+	pk, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	require.NoError(t, err)
+	standard, err := x509.MarshalPKIXPublicKey(&pk.PublicKey)
+	require.NoError(t, err)
+	standard = append(standard, 87)
+	ppk, err := ParsePKIXPublicKey(standard)
+	fmt.Println(err)
+	require.EqualError(t, err, "x509: trailing data after ASN.1 of public-key")
+	require.Nil(t, ppk)
 }
