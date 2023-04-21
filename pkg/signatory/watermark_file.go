49bb32c93472b61e8eb346985def361e7cc7dec0
Eugene Zagidullin <e.asphyx@gmail.com>
2023-04-21 23:58:31 +0300

    stored watermark migration

--- pkg/signatory/watermark_file.go
+++ pkg/signatory/watermark_file.go
@@ -29,13 +29,67 @@ type FileWatermark struct {
 	mtx     sync.Mutex
 }
 
-/*
+type legacyWatermarkData struct {
+	Round int32                          `json:"round,omitempty"`
+	Level int32                          `json:"level"`
+	Hash  tz.Option[tz.BlockPayloadHash] `json:"hash"`
+}
+
+type legacyKindMap map[string]legacyWatermarkMap
+type legacyWatermarkMap = hashmap.PublicKeyHashMap[*legacyWatermarkData]
+
+const legacyWatermarkDir = "watermark"
+
 func (f *FileWatermark) tryLegacy(wm *request.Watermark) (delegateMap, error) {
+	out := make(delegateMap)
 	dir := filepath.Join(f.BaseDir, legacyWatermarkDir)
 	filename := filepath.Join(dir, fmt.Sprintf("%s.json", wm.Chain.String()))
 
+	var kinds legacyKindMap
+	fd, err := os.Open(filename)
+	if err == nil {
+		err = json.NewDecoder(fd).Decode(&kinds)
+		fd.Close()
+		if err != nil {
+			return nil, err
+		}
+	} else if !errors.Is(err, fs.ErrNotExist) {
+		return nil, err
+	} else {
+		return out, nil
+	}
+
+	orders := []struct {
+		kind  string
+		order int
+	}{
+		{"endorsement", request.WmOrderEndorsement},
+		{"preendorsement", request.WmOrderPreendorsement},
+		{"block", request.WmOrderDefault},
+		{"generic", request.WmOrderDefault},
+	}
+
+	for _, o := range orders {
+		if wm, ok := kinds[o.kind]; ok {
+			wm.ForEach(func(pkh tz.PublicKeyHash, data *legacyWatermarkData) bool {
+				stored := request.StoredWatermark{
+					Level: request.Level{
+						Level: data.Level,
+						Round: tz.Some(data.Round),
+					},
+					Order: o.order,
+					Hash:  data.Hash,
+				}
+				if s, ok := out.Get(pkh); !ok || ok && s.Order <= o.order {
+					out.Insert(pkh, &stored)
+				}
+				return true
+			})
+		}
+	}
+
+	return out, nil
 }
-*/
 
 func (f *FileWatermark) IsSafeToSign(pkh crypt.PublicKeyHash, req request.SignRequest, digest *crypt.Digest) error {
 	m, ok := req.(request.WithWatermark)
@@ -64,8 +118,8 @@ func (f *FileWatermark) IsSafeToSign(pkh crypt.PublicKeyHash, req request.SignRe
 		}
 	} else if !errors.Is(err, fs.ErrNotExist) {
 		return err
-	} else {
-		delegates = make(delegateMap)
+	} else if delegates, err = f.tryLegacy(watermark); err != nil {
+		return err
 	}
 
 	if wm, ok := delegates.Get(pkh); ok {
