9f414ece317ce1eba6222e4b479228d015172d01
AbineshECAD <abinesh@ecadlabs.com>
2023-05-02 13:23:45 -0700

    SidebarHeader code alignment change

--- website/sidebars.js
+++ website/sidebars.js
@@ -22,12 +22,22 @@ const sidebars = {
 			className: 'sidebarHeader',
 			collapsed: false,
 			collapsible: false,
-			items: ['start', 'file_based', 'yubihsm', 'azure_kms', 'gcp_kms', 'aws_kms', 'ledger', `cli`, 'remote_policy', 'architecture', 'bakers',
-				{
+			items: ['start',
+					'file_based',
+					'yubihsm',
+					'azure_kms',
+					'gcp_kms',
+					'aws_kms',
+					'ledger',
+					`cli`,
+					'remote_policy',
+					'architecture',
+					'bakers',
+					{
 					type: 'category',
 					label: 'Client Authorization',
 					items: [`authorized_keys`, `jwt`]
-				}],
+					}],
 		},
 	],
 };
