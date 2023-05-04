d1976f691fea690322e62a6f26c161ca77956fb2
stephengaudet <stephen.gaudet@shaw.ca>
2023-05-04 15:43:40 -0700

    sg-flextesa add debug, remove log.Fatal

--- integration_test/authorizedkeys_test.go
+++ integration_test/authorizedkeys_test.go
@@ -19,23 +19,14 @@ func TestAuthorizedKeys(t *testing.T) {
 	require.NotNil(t, err)
 	require.Contains(t, string(out), "remote signer expects authentication signature, but no authorized key was found in the wallet")
 
-	add_auth_key_to_client()
-	defer remove_auth_key_from_client()
+	out, err = OctezClient("import", "secret", "key", "auth", "unencrypted:edsk3ZAm9nqEo7qNugo2wcmxWnbDe7oUUmHt5UJYDdqwucsaHTAsVQ", "--force")
+	defer OctezClient("forget", "address", "auth", "--force")
+	if err != nil {
+		log.Println("failed to import auth key: " + err.Error() + string(out))
+	}
+	require.Nil(t, err)
+
 	out, err = OctezClient("-w", "1", "transfer", "1", "from", "alice", "to", "bob")
 	require.Nil(t, err)
 	require.Contains(t, string(out), "Operation successfully injected in the node.")
-}
-
-func add_auth_key_to_client() {
-	_, err := OctezClient("import", "secret", "key", "auth", "unencrypted:edsk3ZAm9nqEo7qNugo2wcmxWnbDe7oUUmHt5UJYDdqwucsaHTAsVQ")
-	if err != nil {
-		log.Fatal("failed to import auth key: ", err)
-	}
-}
-
-func remove_auth_key_from_client() {
-	_, err := OctezClient("forget", "address", "auth", "--force")
-	if err != nil {
-		log.Fatal("failed to remove auth key: ", err)
-	}
 }
