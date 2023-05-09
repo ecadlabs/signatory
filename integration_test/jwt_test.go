cc333a86a35da9d0a96d4399170f8af2a47475aa
stephengaudet <stephen.gaudet@shaw.ca>
2023-05-09 16:21:15 -0700

    fix jwt integration test

--- integration_test/jwt_test.go
+++ integration_test/jwt_test.go
@@ -13,10 +13,11 @@ import (
 
 const (
 	endpoint = "http://localhost:6732/keys/tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb"
+	login    = "http://localhost:6732/login"
 	message  = "\"03c8e312c61a5fd8e9d6ff1d5dccf900e10b5769e55738eb365e99636e3c3fd1d76c006b82198cb179e8306c1bedd08f12dc863f328886df0202e90700c0843d0000a26828841890d3f3a2a1d4083839c7a882fe050100\""
 )
 
-func TestJWT(t *testing.T) {
+func TestJWTHappyPath(t *testing.T) {
 	//sanity check: request a signature without JWT configured and see it succeed
 	code, bytes := request(endpoint, message, nil)
 	require.Equal(t, 200, code)
@@ -31,11 +32,11 @@ func TestJWT(t *testing.T) {
 	restart_signatory()
 	code, bytes = request(endpoint, message, nil)
 	require.Equal(t, 401, code)
-	assert.Equal(t, "username and password required", string(bytes))
+	assert.Equal(t, "token required", string(bytes))
 
 	//provide credentials in the header of the same request to fetch a bearer token
 	var h = [][]string{{"Content-Type", "application/json"}, {"username", "username1"}, {"password", "password1"}}
-	code, bytes = request(endpoint, message, h)
+	code, bytes = request(login, "", h)
 	require.Equal(t, 201, code)
 	token := string(bytes)
 	require.Greater(t, len(token), 1)
