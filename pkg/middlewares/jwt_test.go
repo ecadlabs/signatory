7152554524400915b00adc4e3410b825d33d1380
AbineshECAD <abinesh@ecadlabs.com>
2023-05-15 22:07:12 -0700

    AuthHandler UT

--- pkg/middlewares/jwt_test.go
+++ pkg/middlewares/jwt_test.go
@@ -21,6 +21,7 @@ func (m *MockAuthGen) GetUserData(user string) (UserData, bool) {
 }
 
 func (m *MockAuthGen) Authenticate(user string, token string) error {
+
 	return nil
 }
 
@@ -38,7 +39,7 @@ func TestLoginHandler(t *testing.T) {
 		password string
 		code     int
 		expected string
-		ma       *MockAuthGen
+		ma       AuthGen
 	}
 
 	cases := testCases{
@@ -146,6 +147,35 @@ func TestLoginHandler(t *testing.T) {
 				},
 			},
 		},
+		{
+			title:    "Generator coverage case",
+			user:     "user",
+			password: "pass",
+			code:     http.StatusCreated,
+			ma: &JWT{
+				Users: map[string]UserData{
+					"user": {
+						Password: "pass",
+						Secret:   "secret",
+					},
+				},
+			},
+		},
+		{
+			title:    "Generator coverage case",
+			user:     "user",
+			password: "pass",
+			code:     http.StatusCreated,
+			ma: &JWT{
+				Users: map[string]UserData{
+					"user": {
+						Password: "pass",
+						Secret:   "secret",
+						Exp:      23,
+					},
+				},
+			},
+		},
 	}
 
 	for _, tc := range cases {
@@ -169,4 +199,132 @@ func TestLoginHandler(t *testing.T) {
 			}
 		})
 	}
+}
+func TestAuthHandlerValidToken(t *testing.T) {
+	user := "user"
+	authGen := &JWT{
+		Users: map[string]UserData{
+			user: {
+				Password: "pass",
+				Secret:   "secret",
+				Exp:      23,
+			},
+		},
+	}
+	jwtMiddleware := NewMiddleware(authGen)
+
+	req, err := http.NewRequest("GET", "/protected", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	tok, err := getToken(user, jwtMiddleware)
+	require.NoError(t, err)
+	req.Header.Set("Authorization", "Bearer "+tok)
+	req.Header.Set("username", user)
+
+	recorder := httptest.NewRecorder()
+
+	handler := jwtMiddleware.AuthHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		u := r.Context().Value("user")
+		fmt.Println("User: ", u)
+		// TBD
+		// require.Equal(t, user, u.(string))
+	}))
+
+	handler.ServeHTTP(recorder, req)
+	require.Equal(t, http.StatusOK, recorder.Code)
+}
+
+func TestAuthHandlerInValidToken(t *testing.T) {
+	user := "user"
+	authGen := &JWT{
+		Users: map[string]UserData{
+			user: {
+				Password: "pass",
+				Secret:   "secret",
+				Exp:      23,
+			},
+		},
+	}
+	jwtMiddleware := NewMiddleware(authGen)
+
+	req, err := http.NewRequest("GET", "/protected", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	tok := "invalid-token"
+	require.NoError(t, err)
+	req.Header.Set("Authorization", "Bearer "+tok)
+	req.Header.Set("username", user)
+
+	recorder := httptest.NewRecorder()
+
+	handler := jwtMiddleware.AuthHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		u := r.Context().Value("user")
+		fmt.Println("User: ", u)
+		// TBD
+		// require.Equal(t, user, u.(string))
+	}))
+
+	handler.ServeHTTP(recorder, req)
+	require.Equal(t, http.StatusUnauthorized, recorder.Code)
+	require.Equal(t, "token contains an invalid number of segments", recorder.Body.String())
+}
+
+func TestAuthHandlerEmptyToken(t *testing.T) {
+	user := "user"
+	authGen := &JWT{
+		Users: map[string]UserData{
+			user: {
+				Password: "pass",
+				Secret:   "secret",
+				Exp:      23,
+			},
+		},
+	}
+	jwtMiddleware := NewMiddleware(authGen)
+
+	req, err := http.NewRequest("GET", "/protected", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	require.NoError(t, err)
+	req.Header.Set("username", user)
+
+	recorder := httptest.NewRecorder()
+
+	handler := jwtMiddleware.AuthHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		u := r.Context().Value("user")
+		fmt.Println("User: ", u)
+		// TBD
+		// require.Equal(t, user, u.(string))
+	}))
+
+	handler.ServeHTTP(recorder, req)
+	require.Equal(t, http.StatusUnauthorized, recorder.Code)
+	require.Equal(t, "token required", recorder.Body.String())
+}
+
+func getToken(user string, au *JWTMiddleware) (string, error) {
+
+	req, err := http.NewRequest("GET", "/login", nil)
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("username", user)
+	ud, b := au.AuthGen.GetUserData(user)
+	if !b {
+		return "", fmt.Errorf("error getting user data")
+	}
+	req.Header.Set("password", ud.Password)
+
+	recorder := httptest.NewRecorder()
+
+	handler := http.HandlerFunc(au.LoginHandler)
+	handler.ServeHTTP(recorder, req)
+
+	if http.StatusCreated != recorder.Code {
+		return "", fmt.Errorf("error generating token")
+	}
+	return recorder.Body.String(), nil
 }
