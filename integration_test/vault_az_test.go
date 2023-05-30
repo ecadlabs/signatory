72e13573ba9bd2a4ba9f52a16d5677bab0994be1
stephengaudet <stephen.gaudet@shaw.ca>
2023-05-30 13:56:07 -0700

    debugging AZ vault

--- integration_test/vault_az_test.go
+++ integration_test/vault_az_test.go
@@ -3,9 +3,6 @@ package integrationtest
 import (
 	"os"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 func TestAZVault(t *testing.T) {
@@ -22,9 +19,6 @@ func TestAZVault(t *testing.T) {
 	tz2 := os.Getenv("VAULT_AZ_TZ2")
 	tz3 := os.Getenv("VAULT_AZ_TZ3")
 
-	tz2alias := "aztz2"
-	tz3alias := "aztz3"
-
 	//config
 	var c Config
 	c.Read()
@@ -38,33 +32,5 @@ func TestAZVault(t *testing.T) {
 	c.Tezos[tz2] = &p
 	c.Tezos[tz3] = &p
 	backup_then_update_config(c)
-	defer restore_config()
-	restart_signatory()
 
-	//setup
-	out, err := OctezClient("import", "secret", "key", tz2alias, "http://signatory:6732/"+tz2)
-	assert.NoError(t, err)
-	assert.Contains(t, string(out), "Tezos address added: "+tz2)
-	defer OctezClient("forget", "address", tz2alias, "--force")
-	out, err = OctezClient("import", "secret", "key", tz3alias, "http://signatory:6732/"+tz3)
-	assert.NoError(t, err)
-	assert.Contains(t, string(out), "Tezos address added: "+tz3)
-	defer OctezClient("forget", "address", tz3alias, "--force")
-
-	out, err = OctezClient("transfer", "100", "from", "alice", "to", tz2alias, "--burn-cap", "0.06425")
-	assert.NoError(t, err)
-	require.Contains(t, string(out), "Operation successfully injected in the node")
-	out, err = OctezClient("transfer", "100", "from", "alice", "to", tz3alias, "--burn-cap", "0.06425")
-	assert.NoError(t, err)
-	require.Contains(t, string(out), "Operation successfully injected in the node")
-
-	//test
-	/* the tz2 key produces invalid signature 50% of the time from octez-client perspective
-	out, err = OctezClient("transfer", "1", "from", tz2alias, "to", "alice", "--burn-cap", "0.06425")
-	assert.NoError(t, err)
-	require.Contains(t, string(out), "Operation successfully injected in the node")
-	*/
-	out, err = OctezClient("transfer", "1", "from", tz3alias, "to", "alice", "--burn-cap", "0.06425")
-	assert.NoError(t, err)
-	require.Contains(t, string(out), "Operation successfully injected in the node")
 }
