f66ad21c4e183f72baa0a77689c5f992e021aa56
stephengaudet <stephen.gaudet@shaw.ca>
2023-06-02 15:24:38 -0700

    add jwt per pkh integration tests

--- integration_test/jwt_test.go
+++ integration_test/jwt_test.go
@@ -15,9 +15,10 @@ import (
 )
 
 const (
+	baseUrl  = "http://localhost:6732/"
 	secret   = "!sEtcU5RwLQYsA5qQ1c6zpo3FljQxfAKP"
-	endpoint = "http://localhost:6732/keys/tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb"
-	login    = "http://localhost:6732/login"
+	endpoint = baseUrl + "keys/tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb"
+	login    = baseUrl + "login"
 	message  = "\"03c8e312c61a5fd8e9d6ff1d5dccf900e10b5769e55738eb365e99636e3c3fd1d76c006b82198cb179e8306c1bedd08f12dc863f328886df0202e90700c0843d0000a26828841890d3f3a2a1d4083839c7a882fe050100\""
 )
 
@@ -27,7 +28,7 @@ func TestJWTHappyPath(t *testing.T) {
 	require.Equal(t, 200, code)
 	require.Contains(t, string(bytes), "signature")
 
-	//configure JWT and make the same request, and see it fail
+	//configure JWT and make the same request, and see it fail because you have no token
 	var c Config
 	c.Read()
 	c.Server.Jwt = JwtConfig{Users: map[string]*JwtUserData{"username1": {Password: "password1", Secret: secret, Exp: 60}}}
@@ -38,7 +39,7 @@ func TestJWTHappyPath(t *testing.T) {
 	require.Equal(t, 401, code)
 	assert.Equal(t, "token required", string(bytes))
 
-	//provide credentials in the header of the same request to fetch a bearer token
+	//get a bearer token from the login endpoint
 	var h = [][]string{{"Content-Type", "application/json"}, {"username", "username1"}, {"password", "password1"}}
 	code, bytes = request(login, "", h)
 	require.Equal(t, 201, code)
@@ -48,7 +49,7 @@ func TestJWTHappyPath(t *testing.T) {
 	require.Equal(t, 2, strings.Count(token, "."))
 	fmt.Println(token)
 
-	//request a signature with a token
+	//request with a token is successful
 	h = [][]string{{"Content-Type", "application/json"}, {"username", "username1"}, {"Authorization", "Bearer " + token}}
 	code, bytes = request(endpoint, message, h)
 	require.Equal(t, 200, code)
@@ -290,4 +291,93 @@ func TestPasswordRotation(t *testing.T) {
 	token = string(bytes)
 	assert.NotContains(t, token, "signature")
 	assert.Equal(t, 2, strings.Count(token, "."))
+}
+
+func TestPerPkh(t *testing.T) {
+	var pkh1 = "tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb"
+	var pkh2 = "tz1aSkwEot3L2kmUvcoxzjMomb9mvBNuzFK6"
+	var pkh3 = "tz1RKGhRF4TZNCXEfwyqZshGsVfrZeVU446B"
+	var pkh4 = "tz1R8HJMzVdZ9RqLCknxeq9w5rSbiqJ41szi"
+	var base = baseUrl + "keys/"
+	var url1 = base + pkh1
+	var url2 = base + pkh2
+	var url3 = base + pkh3
+	var url4 = base + pkh4
+
+	var c Config
+	c.Read()
+	c.Server.Jwt = JwtConfig{Users: map[string]*JwtUserData{"username1": {Password: "password1", Secret: secret},
+		"username2": {Password: "password2", Secret: secret}}}
+
+	c.Tezos[pkh1].JwtUsers = []string{"username1"}
+	c.Tezos[pkh2].JwtUsers = []string{"username2"}
+	c.Tezos[pkh3].JwtUsers = []string{"username1", "username2"}
+	c.Tezos[pkh3].Allow = map[string][]string{"generic": {"transaction"}}
+	c.Tezos[pkh4].Allow = map[string][]string{"generic": {"transaction"}}
+
+	backup_then_update_config(c)
+	defer restore_config()
+	restart_signatory()
+
+	//user1 login
+	var h = [][]string{{"Content-Type", "application/json"}, {"username", "username1"}, {"password", "password1"}}
+	code, bytes := request(login, "", h)
+	require.Equal(t, 201, code)
+	token1 := string(bytes)
+
+	//user2 login
+	h = [][]string{{"Content-Type", "application/json"}, {"username", "username2"}, {"password", "password2"}}
+	code, bytes = request(login, "", h)
+	require.Equal(t, 201, code)
+	token2 := string(bytes)
+
+	//pkh1 signs for user1
+	h = [][]string{{"Content-Type", "application/json"}, {"username", "username1"}, {"Authorization", "Bearer " + token1}}
+	code, bytes = request(url1, message, h)
+	assert.Equal(t, 200, code)
+	assert.Contains(t, string(bytes), "signature")
+
+	//pkh1 does not sign for user2
+	h = [][]string{{"Content-Type", "application/json"}, {"username", "username2"}, {"Authorization", "Bearer " + token2}}
+	code, bytes = request(url1, message, h)
+	assert.Equal(t, 403, code)
+	assert.Contains(t, string(bytes), "user `username2' is not authorized to access "+pkh1)
+
+	//ISSUE #372 pkh1 does not sign for malicious user2 who tries to be user1
+	h = [][]string{{"Content-Type", "application/json"}, {"username", "username1"}, {"Authorization", "Bearer " + token2}}
+	code, bytes = request(url1, message, h)
+	assert.Equal(t, 403, code)
+	assert.Contains(t, string(bytes), "user `username2' is not authorized to access "+pkh1)
+
+	//pkh2 signs for user2
+	h = [][]string{{"Content-Type", "application/json"}, {"username", "username2"}, {"Authorization", "Bearer " + token2}}
+	code, bytes = request(url2, message, h)
+	assert.Equal(t, 200, code)
+	assert.Contains(t, string(bytes), "signature")
+
+	//pkh2 does not sign for user1
+	h = [][]string{{"Content-Type", "application/json"}, {"username", "username1"}, {"Authorization", "Bearer " + token1}}
+	code, bytes = request(url2, message, h)
+	assert.Equal(t, 403, code)
+	assert.Contains(t, string(bytes), "user `username1' is not authorized to access "+pkh2)
+
+	//pkh3 signs for both user1 and user2 because both are configured
+	h = [][]string{{"Content-Type", "application/json"}, {"username", "username1"}, {"Authorization", "Bearer " + token1}}
+	code, bytes = request(url3, message, h)
+	assert.Equal(t, 200, code)
+	assert.Contains(t, string(bytes), "signature")
+	h = [][]string{{"Content-Type", "application/json"}, {"username", "username2"}, {"Authorization", "Bearer " + token2}}
+	code, bytes = request(url3, message, h)
+	assert.Equal(t, 200, code)
+	assert.Contains(t, string(bytes), "signature")
+
+	//pkh4 signs for both user1 and user2 because nobody is configured
+	h = [][]string{{"Content-Type", "application/json"}, {"username", "username1"}, {"Authorization", "Bearer " + token1}}
+	code, bytes = request(url4, message, h)
+	assert.Equal(t, 200, code)
+	assert.Contains(t, string(bytes), "signature")
+	h = [][]string{{"Content-Type", "application/json"}, {"username", "username2"}, {"Authorization", "Bearer " + token2}}
+	code, bytes = request(url4, message, h)
+	assert.Equal(t, 200, code)
+	assert.Contains(t, string(bytes), "signature")
 }
