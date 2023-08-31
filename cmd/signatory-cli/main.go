f111fee27afea79e31cf18391a67ece320cd27e0
Dzianis Andreyenka <andreenkodn@gmail.com>
2023-08-31 11:09:18 +0200

    init HCP for signatory CLI
    Signed-off-by: Dzianis Andreyenka <andreenkodn@gmail.com>

--- cmd/signatory-cli/main.go
+++ cmd/signatory-cli/main.go
@@ -16,6 +16,7 @@ import (
 	_ "github.com/ecadlabs/signatory/pkg/vault/azure"
 	_ "github.com/ecadlabs/signatory/pkg/vault/cloudkms"
 	_ "github.com/ecadlabs/signatory/pkg/vault/file"
+	_ "github.com/ecadlabs/signatory/pkg/vault/hashicorp"
 	_ "github.com/ecadlabs/signatory/pkg/vault/ledger"
 	_ "github.com/ecadlabs/signatory/pkg/vault/mem"
 	_ "github.com/ecadlabs/signatory/pkg/vault/yubi"
