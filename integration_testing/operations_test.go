6768e11027a4d33916752ff298990b0d46a5bc0b
Eugene Zagidullin <e.asphyx@gmail.com>
2024-10-30 20:18:36 +0200

    test different key types

--- integration_testing/operations_test.go
+++ integration_testing/operations_test.go
@@ -2,12 +2,15 @@ package integrationtesting
 
 import (
 	"context"
+	"crypto/ecdsa"
 	"crypto/ed25519"
+	"crypto/elliptic"
 	"crypto/rand"
 	"regexp"
 	"strings"
 	"testing"
 
+	"github.com/decred/dcrd/dcrec/secp256k1/v4"
 	"github.com/ecadlabs/gotez/v2/crypt"
 	"github.com/ecadlabs/signatory/integration_testing/tezbox"
 	"github.com/ecadlabs/signatory/pkg/config"
@@ -23,15 +26,17 @@ import (
 )
 
 const (
-	signatoryAlias      = "signatory"
-	signatoryBakerAlias = "signatory_baker"
-	contractAlias       = "emit_event"
+	tz1Alias      = "signatory_tz1"
+	tz2Alias      = "signatory_tz2"
+	tz3Alias      = "signatory_tz3"
+	bakerAlias    = "signatory_baker"
+	contractAlias = "emit_event"
 )
 
 type opTest struct {
-	kind        string
-	clientArgs  func(alias string) []string
-	okMessageRe func(alias string) string
+	name        string
+	clientArgs  []string
+	okMessageRe string
 }
 
 const contractBody = `{
@@ -67,57 +72,49 @@ func replaceWhiteSpaces(s string) string {
 
 var opTests = []*opTest{
 	{
-		kind: "reveal",
-		clientArgs: func(alias string) []string {
-			return []string{"reveal", "key", "for", alias}
-		},
+		name:       "reveal",
+		clientArgs: []string{"reveal", "key", "for", tz1Alias},
 	},
 	{
-		kind: "register_global_constant",
-		clientArgs: func(alias string) []string {
-			return []string{"register", "global", "constant", "999", "from", alias, "--burn-cap", "0.017"}
-		},
+		name:       "register_global_constant",
+		clientArgs: []string{"register", "global", "constant", "999", "from", tz1Alias, "--burn-cap", "0.017"},
 	},
 	{
-		kind: "transaction",
-		clientArgs: func(alias string) []string {
-			return []string{"transfer", "1", "from", alias, "to", "alice", "--burn-cap", "0.06425"}
-		},
+		name:       "transaction-tz1",
+		clientArgs: []string{"transfer", "1", "from", tz1Alias, "to", "alice", "--burn-cap", "0.06425"},
 	},
 	{
-		kind: "delegation",
-		clientArgs: func(alias string) []string {
-			return []string{"register", "key", alias, "as", "delegate"}
-		},
+		name:       "transaction-tz2",
+		clientArgs: []string{"transfer", "1", "from", tz2Alias, "to", "alice", "--burn-cap", "0.06425"},
 	},
 	{
-		kind: "set_deposits_limit",
-		clientArgs: func(alias string) []string {
-			return []string{"set", "deposits", "limit", "for", alias, "to", "10000"}
-		},
+		name:       "transaction-tz3",
+		clientArgs: []string{"transfer", "1", "from", tz3Alias, "to", "alice", "--burn-cap", "0.06425"},
 	},
 	{
-		kind: "update_consensus_key",
-		clientArgs: func(alias string) []string {
-			return []string{"set", "consensus", "key", "for", alias, "to", "bob"}
-		},
+		name:       "delegation",
+		clientArgs: []string{"register", "key", tz1Alias, "as", "delegate"},
 	},
 	{
-		kind: "origination",
-		clientArgs: func(alias string) []string {
-			return []string{"originate", "contract", contractAlias, "transferring", "1", "from", alias, "running", replaceWhiteSpaces(contractBody), "--burn-cap", "0.4"}
-		},
+		name:       "set_deposits_limit",
+		clientArgs: []string{"set", "deposits", "limit", "for", tz1Alias, "to", "10000"},
 	},
 	{
-		kind: "increase_paid_storage",
-		clientArgs: func(alias string) []string {
-			return []string{"increase", "the", "paid", "storage", "of", contractAlias, "by", "0x5c", "bytes", "from", alias}
-		},
+		name:       "update_consensus_key",
+		clientArgs: []string{"set", "consensus", "key", "for", tz1Alias, "to", "bob"},
+	},
+	{
+		name:       "origination",
+		clientArgs: []string{"originate", "contract", contractAlias, "transferring", "1", "from", tz1Alias, "running", replaceWhiteSpaces(contractBody), "--burn-cap", "0.4"},
+	},
+	{
+		name:       "increase_paid_storage",
+		clientArgs: []string{"increase", "the", "paid", "storage", "of", contractAlias, "by", "0x5c", "bytes", "from", tz1Alias},
 	},
 }
 
-func genKeys(n int) ([]crypt.PrivateKey, error) {
-	out := make([]crypt.PrivateKey, n)
+func genEd25519Keys(n int) ([]crypt.Ed25519PrivateKey, error) {
+	out := make([]crypt.Ed25519PrivateKey, n)
 	for i := 0; i < n; i++ {
 		_, k, err := ed25519.GenerateKey(rand.Reader)
 		if err != nil {
@@ -129,21 +126,31 @@ func genKeys(n int) ([]crypt.PrivateKey, error) {
 }
 
 func TestOperations(t *testing.T) {
-	priv, err := genKeys(2)
+	priv1, err := genEd25519Keys(2)
 	require.NoError(t, err)
+
+	k2, err := ecdsa.GenerateKey(secp256k1.S256(), rand.Reader)
+	require.NoError(t, err)
+	priv2 := (*crypt.ECDSAPrivateKey)(k2)
+
+	k3, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	require.NoError(t, err)
+	priv3 := (*crypt.ECDSAPrivateKey)(k3)
 
 	conf := signatory.Config{
 		Vaults:    map[string]*config.VaultConfig{"mem": {Driver: "mem"}},
 		Watermark: watermark.Ignore{},
 		VaultFactory: vault.FactoryFunc(func(ctx context.Context, name string, conf *yaml.Node) (vault.Vault, error) {
 			return memory.New([]*memory.PrivateKey{
-				{PrivateKey: priv[0]},
-				{PrivateKey: priv[1]},
+				{PrivateKey: priv1[0]},
+				{PrivateKey: priv1[1]},
+				{PrivateKey: priv2},
+				{PrivateKey: priv3},
 			}, "")
 		}),
 		Policy: hashmap.NewPublicKeyHashMap([]hashmap.PublicKeyKV[*signatory.PublicKeyPolicy]{
 			{
-				Key: priv[0].Public().Hash(),
+				Key: priv1[0].Public().Hash(),
 				Val: &signatory.PublicKeyPolicy{
 					AllowedRequests: []string{"generic"},
 					AllowedOps:      opKinds(),
@@ -151,9 +158,26 @@ func TestOperations(t *testing.T) {
 				},
 			},
 			{
-				Key: priv[1].Public().Hash(),
+				Key: priv2.Public().Hash(),
 				Val: &signatory.PublicKeyPolicy{
-					AllowedRequests: []string{"block", "attestation", "preattestation"},
+					AllowedRequests: []string{"generic"},
+					AllowedOps:      opKinds(),
+					LogPayloads:     true,
+				},
+			},
+			{
+				Key: priv3.Public().Hash(),
+				Val: &signatory.PublicKeyPolicy{
+					AllowedRequests: []string{"generic"},
+					AllowedOps:      opKinds(),
+					LogPayloads:     true,
+				},
+			},
+			{
+				Key: priv1[1].Public().Hash(),
+				Val: &signatory.PublicKeyPolicy{
+					AllowedRequests: []string{"block", "attestation", "preattestation", "generic"},
+					AllowedOps:      opKinds(),
 					LogPayloads:     true,
 				},
 			},
@@ -178,8 +202,13 @@ func TestOperations(t *testing.T) {
 	tezboxConfig, err := genBaseConfig()
 	require.NoError(t, err)
 
-	tezboxConfig.Accounts.Regular[signatoryAlias] = newRemoteSignerConfig(priv[0].Public(), l.Addr(), regularBalance)
-	tezboxConfig.Accounts.Bakers[signatoryBakerAlias] = newRemoteSignerConfig(priv[1].Public(), l.Addr(), bakerBalance)
+	tezboxConfig.Accounts.Regular[tz1Alias] = newRemoteSignerConfig(priv1[0].Public(), l.Addr(), regularBalance)
+	tezboxConfig.Accounts.Regular[tz2Alias] = newRemoteSignerConfig(priv2.Public(), l.Addr(), regularBalance)
+	tezboxConfig.Accounts.Regular[tz3Alias] = newRemoteSignerConfig(priv3.Public(), l.Addr(), regularBalance)
+
+	tezboxConfig.Accounts.Bakers = tezbox.Accounts{
+		bakerAlias: newRemoteSignerConfig(priv1[1].Public(), l.Addr(), bakerBalance),
+	}
 
 	cont, err := tezbox.Start(tezboxConfig)
 	require.NoError(t, err)
@@ -188,14 +217,13 @@ func TestOperations(t *testing.T) {
 	})
 
 	for _, test := range opTests {
-		t.Run(test.kind, func(t *testing.T) {
-			a := test.clientArgs(signatoryAlias)
-			log.Infof("octez-client arguments: %s", strings.Join(a, " "))
-			out, err := cont.Exec("octez-client", test.clientArgs(signatoryAlias)...)
+		t.Run(test.name, func(t *testing.T) {
+			log.Infof("octez-client arguments: %s", strings.Join(test.clientArgs, " "))
+			out, err := cont.Exec("octez-client", test.clientArgs...)
 			log.Info(string(out))
 			require.NoError(t, err)
-			if test.okMessageRe != nil {
-				matched, err := regexp.Match(test.okMessageRe(signatoryAlias), out)
+			if test.okMessageRe != "" {
+				matched, err := regexp.Match(test.okMessageRe, out)
 				require.NoError(t, err)
 				require.True(t, matched)
 			}
