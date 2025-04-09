e15cd47ab0b0bcbbdf723aa8e93eb4f6d91a6d2d
deterclosed <164524498+deterclosed@users.noreply.github.com>
2025-04-09 11:40:07 +0800

    chore: fix some function names in comment (#513)
    Signed-off-by: deterclosed <fliter@outlook.com>
    Co-authored-by: Jev Björsell <jev@ecadlabs.com>

--- pkg/cryptoutils/pkcs.go
+++ pkg/cryptoutils/pkcs.go
@@ -96,7 +96,7 @@ func marshalPKCS8ECDSAPrivateKey(key *ecdsa.PrivateKey) ([]byte, error) {
 	return asn1.Marshal(privKey)
 }
 
-// marshalECPrivateKey marshals an EC private key into ASN.1, DER format and
+// marshalECPrivateKeyWithOID marshals an EC private key into ASN.1, DER format and
 // sets the curve ID to the given OID, or omits it if OID is nil.
 func marshalECPrivateKeyWithOID(key *ecdsa.PrivateKey, oid asn1.ObjectIdentifier) ([]byte, error) {
 	privateKeyBytes := key.D.Bytes()
