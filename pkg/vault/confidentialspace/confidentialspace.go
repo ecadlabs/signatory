bf3ecd4fb50f09f76d6de01b8062248d43c776a4
Petel__ <petelauck@gmail.com>
2025-07-24 19:04:05 -0700

    add GCP confidential space vault

---
+++ pkg/vault/confidentialspace/confidentialspace.go
@@ -0,0 +1,385 @@
+package confidentialspace
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"iter"
+	"net"
+	"os"
+	"path/filepath"
+	"sync"
+
+	tz "github.com/ecadlabs/gotez/v2"
+	"github.com/ecadlabs/gotez/v2/crypt"
+	"github.com/ecadlabs/signatory/pkg/config"
+	"github.com/ecadlabs/signatory/pkg/cryptoutils"
+	"github.com/ecadlabs/signatory/pkg/utils"
+	"github.com/ecadlabs/signatory/pkg/vault"
+	"github.com/ecadlabs/signatory/pkg/vault/confidentialspace/rpc"
+	log "github.com/sirupsen/logrus"
+	"gopkg.in/yaml.v3"
+)
+
+const (
+	DefaultPort = 2000
+)
+
+const defaultFile = "confidential_space_keys.json"
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+type StorageConfig struct {
+	Driver string    `yaml:"driver"`
+	Config yaml.Node `yaml:"config"`
+}
+
+type result[T any] interface {
+	Result() iter.Seq[T]
+	Err() error
+}
+
+type encryptedKey struct {
+	PublicKeyHash       tz.PublicKeyHash `json:"public_key_hash"`
+	EncryptedPrivateKey []byte           `json:"encrypted_private_key"`
+}
+
+type keyBlobStorage interface {
+	GetKeys(ctx context.Context) (result[*encryptedKey], error)
+	ImportKey(ctx context.Context, encryptedKey *encryptedKey) error
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+type Config struct {
+	ConfidentialSpaceHost string         `yaml:"confidential_space_host"`
+	ConfidentialSpacePort string         `yaml:"confidential_space_port"`
+	WipPath               string         `yaml:"wip_path"`
+	EncryptionKeyPath     string         `yaml:"encryption_key_path"`
+	Storage               *StorageConfig `yaml:"storage"`
+}
+
+func resolve[T comparable](value T, ev string) T {
+	var zero T
+	if value == zero {
+		if env := os.Getenv(ev); env != "" {
+			var tmp T
+			if _, err := fmt.Sscanf(env, "%v", &tmp); err == nil {
+				return tmp
+			}
+		}
+	}
+	return value
+}
+
+func resolvePtr[T any](value *T, ev string) *T {
+	if value == nil {
+		if env := os.Getenv(ev); env != "" {
+			var tmp T
+			if _, err := fmt.Sscanf(env, "%v", &tmp); err == nil {
+				return &tmp
+			}
+		}
+	}
+	return value
+}
+
+func populateConfig(c *Config) *Config {
+	if c == nil {
+		var zero Config
+		c = &zero
+	}
+	return &Config{
+		ConfidentialSpaceHost: resolve(c.ConfidentialSpaceHost, "CONFIDENTIAL_SPACE_HOST"),
+		ConfidentialSpacePort: resolve(c.ConfidentialSpacePort, "CONFIDENTIAL_SPACE_PORT"),
+		WipPath:               resolve(c.WipPath, "GCP_WIP_PATH"),
+		EncryptionKeyPath:     resolve(c.EncryptionKeyPath, "GCP_KMS_ENCRYPTION_KEY_PATH"),
+		Storage:               c.Storage,
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+type ConfidentialSpaceVault[C any] struct {
+	client  *rpc.Client[C]
+	storage keyBlobStorage
+	keys    []*confidentialKey
+	mtx     sync.Mutex
+}
+
+func (v *ConfidentialSpaceVault[C]) List(ctx context.Context) vault.KeyIterator {
+	v.mtx.Lock()
+	defer v.mtx.Unlock()
+
+	snap := v.keys
+	i := 0
+	return vault.IteratorFunc(func() (key vault.KeyReference, err error) {
+		if i >= len(snap) {
+			return nil, vault.ErrDone
+		}
+		k := &confidentialKeyRef[C]{
+			confidentialKey: snap[i],
+			v:               v,
+		}
+		i++
+		return k, nil
+	})
+}
+
+func (v *ConfidentialSpaceVault[C]) Close(ctx context.Context) (err error) {
+	err = v.client.Close()
+	return err
+}
+
+func (v *ConfidentialSpaceVault[C]) Name() string { return "ConfidentialSpace" }
+
+func (v *ConfidentialSpaceVault[C]) Import(ctx context.Context, pk crypt.PrivateKey, opt utils.Options) (vault.KeyReference, error) {
+	rpcPk, err := rpc.NewPrivateKey(pk)
+	if err != nil {
+		return nil, fmt.Errorf("(ConfidentialSpace): %w", err)
+	}
+
+	v.mtx.Lock()
+	defer v.mtx.Unlock()
+
+	res, err := v.client.ImportUnencrypted(ctx, rpcPk)
+	if err != nil {
+		return nil, fmt.Errorf("(ConfidentialSpace): %w", err)
+	}
+	p, err := res.PublicKey.PublicKey()
+	if err != nil {
+		return nil, fmt.Errorf("(ConfidentialSpace): %w", err)
+	}
+	key := &confidentialKey{
+		pub:    p,
+		handle: res.Handle,
+	}
+	v.keys = append(v.keys, key)
+
+	if err := v.storage.ImportKey(ctx, &encryptedKey{
+		PublicKeyHash:       p.Hash(),
+		EncryptedPrivateKey: res.EncryptedPrivateKey,
+	}); err != nil {
+		return nil, fmt.Errorf("(ConfidentialSpace): %w", err)
+	}
+
+	return &confidentialKeyRef[C]{
+		confidentialKey: key,
+		v:               v,
+	}, nil
+}
+
+func (v *ConfidentialSpaceVault[C]) Generate(ctx context.Context, keyType *cryptoutils.KeyType, n int) (vault.KeyIterator, error) {
+	var kt rpc.KeyType
+	switch keyType {
+	case cryptoutils.KeyEd25519:
+		kt = rpc.KeyEd25519
+	case cryptoutils.KeySecp256k1:
+		kt = rpc.KeySecp256k1
+	case cryptoutils.KeyP256:
+		kt = rpc.KeyNISTP256
+	case cryptoutils.KeyBLS12_381:
+		kt = rpc.KeyBLS
+	default:
+		return nil, fmt.Errorf("(ConfidentialSpace): unsupported key type %v", keyType)
+	}
+
+	v.mtx.Lock()
+	defer v.mtx.Unlock()
+
+	var imported []*confidentialKey
+	for i := 0; i < n; i++ {
+		res, err := v.client.GenerateAndImport(ctx, kt)
+		if err != nil {
+			return nil, fmt.Errorf("(ConfidentialSpace): %w", err)
+		}
+		p, err := res.PublicKey.PublicKey()
+		if err != nil {
+			return nil, fmt.Errorf("(ConfidentialSpace): %w", err)
+		}
+		key := &confidentialKey{
+			pub:    p,
+			handle: res.Handle,
+		}
+		v.keys = append(v.keys, key)
+		if err := v.storage.ImportKey(ctx, &encryptedKey{
+			PublicKeyHash:       p.Hash(),
+			EncryptedPrivateKey: res.EncryptedPrivateKey,
+		}); err != nil {
+			return nil, fmt.Errorf("(ConfidentialSpace): %w", err)
+		}
+		imported = append(imported, key)
+	}
+
+	i := 0
+	return vault.IteratorFunc(func() (key vault.KeyReference, err error) {
+		if i >= len(imported) {
+			return nil, vault.ErrDone
+		}
+		k := &confidentialKeyRef[C]{
+			confidentialKey: imported[i],
+			v:               v,
+		}
+		i++
+		return k, nil
+	}), nil
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+type confidentialKey struct {
+	pub    crypt.PublicKey
+	handle uint64
+}
+
+type confidentialKeyRef[C any] struct {
+	*confidentialKey
+	v *ConfidentialSpaceVault[C]
+}
+
+func (r *confidentialKeyRef[C]) PublicKey() crypt.PublicKey { return r.pub }
+func (r *confidentialKeyRef[C]) Vault() vault.Vault         { return r.v }
+
+func (r *confidentialKeyRef[C]) Sign(ctx context.Context, message []byte) (crypt.Signature, error) {
+	r.v.mtx.Lock()
+	defer r.v.mtx.Unlock()
+
+	sig, err := r.v.client.Sign(ctx, r.handle, message)
+	if err != nil {
+		return nil, fmt.Errorf("(ConfidentialSpace): %w", err)
+	}
+
+	res, err := sig.Signature()
+	if err != nil {
+		return nil, fmt.Errorf("(ConfidentialSpace): %w", err)
+	}
+	return res, nil
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+func New(ctx context.Context, config *Config, global config.GlobalContext) (*ConfidentialSpaceVault[rpc.ConfidentialSpaceCredentials], error) {
+	var sc *StorageConfig
+	if config != nil {
+		sc = config.Storage
+	}
+	storage, err := newStorage(ctx, sc, global)
+	if err != nil {
+		return nil, fmt.Errorf("(ConfidentialSpace): %w", err)
+	}
+	return newWithStorage(ctx, config, storage)
+}
+
+func newStorage(ctx context.Context, conf *StorageConfig, global config.GlobalContext) (keyBlobStorage, error) {
+	if conf != nil {
+		switch conf.Driver {
+		case "file":
+			var path string
+			if conf.Config.IsZero() {
+				path = filepath.Join(global.GetBaseDir(), defaultFile)
+			} else if err := conf.Config.Decode(&path); err == nil {
+				path = os.ExpandEnv(path)
+			} else {
+				return nil, err
+			}
+			return newFileStorage(path)
+		default:
+			return nil, fmt.Errorf("unknown key storage %s", conf.Driver)
+		}
+	} else {
+		path := filepath.Join(global.GetBaseDir(), defaultFile)
+		return newFileStorage(path)
+	}
+}
+
+func newWithStorage(ctx context.Context, config *Config, storage keyBlobStorage) (*ConfidentialSpaceVault[rpc.ConfidentialSpaceCredentials], error) {
+	conf := populateConfig(config)
+
+	rpcCred := rpc.ConfidentialSpaceCredentials{
+		WipPath:           conf.WipPath,
+		EncryptionKeyPath: conf.EncryptionKeyPath,
+	}
+
+	if conf.EncryptionKeyPath == "" {
+		return nil, errors.New("(ConfidentialSpace): missing encryption key id")
+	}
+	if !rpcCred.IsValid() {
+		return nil, errors.New("(ConfidentialSpace): missing credentials")
+	}
+
+	addr := net.JoinHostPort(conf.ConfidentialSpaceHost, conf.ConfidentialSpacePort)
+	log.Infof("(ConfidentialSpace): connecting to the enclave signer on %v...", addr)
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		return nil, fmt.Errorf("(ConfidentialSpace): %w", err)
+	}
+
+	v, err := newWithConn(ctx, conn, &rpcCred, storage)
+	if err != nil {
+		return nil, err
+	}
+	return v, nil
+}
+
+func newWithConn[C any](ctx context.Context, conn net.Conn, credentials *C, storage keyBlobStorage) (*ConfidentialSpaceVault[C], error) {
+	client := rpc.NewClient[C](conn)
+	client.Logger = log.StandardLogger()
+
+	if err := client.Initialize(ctx, credentials); err != nil {
+		return nil, fmt.Errorf("(ConfidentialSpace): %w", err)
+	}
+
+	// populate from storage
+	r, err := storage.GetKeys(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("(ConfidentialSpace): %w", err)
+	}
+
+	var keys []*confidentialKey
+	for k := range r.Result() {
+		log.WithField("pkh", k.PublicKeyHash).Debug("Importing encrypted key")
+		res, err := client.Import(ctx, k.EncryptedPrivateKey)
+		if err != nil {
+			return nil, fmt.Errorf("(ConfidentialSpace): %w", err)
+		}
+		p, err := res.PublicKey.PublicKey()
+		if err != nil {
+			return nil, fmt.Errorf("(ConfidentialSpace): %w", err)
+		}
+		keys = append(keys, &confidentialKey{
+			pub:    p,
+			handle: res.Handle,
+		})
+	}
+	if err := r.Err(); err != nil {
+		return nil, fmt.Errorf("(ConfidentialSpace): %w", err)
+	}
+
+	return &ConfidentialSpaceVault[C]{
+		client:  client,
+		storage: storage,
+		keys:    keys,
+	}, nil
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+func init() {
+	vault.RegisterVault("confidentialspace", func(ctx context.Context, node *yaml.Node, global config.GlobalContext) (vault.Vault, error) {
+		var conf *Config
+		if node != nil && !node.IsZero() {
+			conf = &Config{}
+			if err := node.Decode(conf); err != nil {
+				return nil, err
+			}
+		}
+		return New(ctx, conf, global)
+	})
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+var (
+	_ vault.Importer  = (*ConfidentialSpaceVault[rpc.ConfidentialSpaceCredentials])(nil)
+	_ vault.Generator = (*ConfidentialSpaceVault[rpc.ConfidentialSpaceCredentials])(nil)
+)
---
+++ pkg/vault/confidentialspace/rpc/types.go
@@ -0,0 +1,229 @@
+package rpc
+
+import (
+	"crypto/ed25519"
+	"crypto/elliptic"
+	"errors"
+	"fmt"
+	"math/big"
+
+	"github.com/ecadlabs/goblst"
+	"github.com/ecadlabs/goblst/minpk"
+	"github.com/ecadlabs/gotez/v2/crypt"
+)
+
+type KeyType string
+
+const (
+	KeySecp256k1 KeyType = "Secp256k1"
+	KeyNISTP256  KeyType = "NistP256"
+	KeyEd25519   KeyType = "Ed25519"
+	KeyBLS       KeyType = "Bls"
+)
+
+type Protected string
+
+// prevent leakage via "%#v" format used for debugging
+func (p Protected) GoString() string {
+	if p != "" {
+		return "\"(FILTERED)\""
+	}
+	return "\"\""
+}
+
+type ConfidentialSpaceCredentials struct {
+	WipPath           string `cbor:"wip_path"`
+	EncryptionKeyPath string `cbor:"encryption_key_path"`
+}
+
+func (c *ConfidentialSpaceCredentials) IsValid() bool {
+	return c.WipPath != "" && c.EncryptionKeyPath != ""
+}
+
+type SignRequest struct {
+	Handle  uint64 `cbor:"handle"`
+	Message []byte `cbor:"message"`
+}
+
+type SignWithRequest struct {
+	EncryptedPrivateKey []byte `cbor:"encrypted_private_key"`
+	Message             []byte `cbor:"message"`
+}
+
+type Request[C any] struct {
+	Initialize        *C               `cbor:"Initialize,omitempty"`
+	Import            []byte           `cbor:"Import,omitempty"`
+	ImportUnencrypted *PrivateKey      `cbor:"ImportUnencrypted,omitempty"`
+	Generate          *KeyType         `cbor:"Generate,omitempty"`
+	GenerateAndImport *KeyType         `cbor:"GenerateAndImport,omitempty"`
+	Sign              *SignRequest     `cbor:"Sign,omitempty"`
+	SignWith          *SignWithRequest `cbor:"SignWith,omitempty"`
+	PublicKey         *uint64          `cbor:"PublicKey,omitempty"`
+	PublicKeyFrom     []byte           `cbor:"PublicKeyFrom,omitempty"`
+	ProvePossession   *uint64          `cbor:"ProvePossession,omitempty"`
+}
+
+type PublicKey struct {
+	Secp256k1 []byte `cbor:"Secp256k1,omitempty"`
+	P256      []byte `cbor:"NistP256,omitempty"`
+	Ed25519   []byte `cbor:"Ed25519,omitempty"`
+	BLS       []byte `cbor:"Bls,omitempty"`
+}
+
+func (p *PublicKey) PublicKey() (crypt.PublicKey, error) {
+	switch {
+	case p.Secp256k1 != nil || p.P256 != nil:
+		if p.Secp256k1 != nil {
+			return crypt.UnmarshalECDSAPublicKey(p.Secp256k1, crypt.S256())
+		} else {
+			return crypt.UnmarshalECDSAPublicKey(p.P256, elliptic.P256())
+		}
+
+	case p.Ed25519 != nil:
+		if len(p.Ed25519) != ed25519.PublicKeySize {
+			return nil, errors.New("invalid ed25519 public key length")
+		}
+		return crypt.Ed25519PublicKey(p.Ed25519), nil
+
+	case p.BLS != nil:
+		p, err := minpk.PublicKeyFromBytes(p.BLS)
+		if err != nil {
+			return nil, err
+		}
+		return (*crypt.BLSPublicKey)(p), nil
+
+	default:
+		return nil, errors.New("malformed public key RPC response")
+	}
+}
+
+type PrivateKey struct {
+	Secp256k1 []byte `cbor:"Secp256k1,omitempty"`
+	P256      []byte `cbor:"NistP256,omitempty"`
+	Ed25519   []byte `cbor:"Ed25519,omitempty"`
+	BLS       []byte `cbor:"Bls,omitempty"`
+}
+
+func NewPrivateKey(priv crypt.PrivateKey) (*PrivateKey, error) {
+	switch priv := priv.(type) {
+	case *crypt.ECDSAPrivateKey:
+		data := make([]byte, (priv.Params().BitSize+7)/8)
+		priv.D.FillBytes(data)
+		switch priv.Curve {
+		case elliptic.P256():
+			return &PrivateKey{P256: data}, nil
+		case crypt.S256():
+			return &PrivateKey{Secp256k1: data}, nil
+		default:
+			return nil, fmt.Errorf("unsupported curve %T", priv.Curve)
+		}
+
+	case crypt.Ed25519PrivateKey:
+		return &PrivateKey{Ed25519: priv.Seed()}, nil
+
+	case *crypt.BLSPrivateKey:
+		return &PrivateKey{BLS: (*goblst.Scalar)(priv).BEBytes()}, nil
+
+	default:
+		return nil, fmt.Errorf("unsupported key type %T", priv)
+	}
+}
+
+type Signature struct {
+	Secp256k1 []byte `cbor:"Secp256k1,omitempty"`
+	P256      []byte `cbor:"NistP256,omitempty"`
+	Ed25519   []byte `cbor:"Ed25519,omitempty"`
+	BLS       []byte `cbor:"Bls,omitempty"`
+}
+
+func point(src []byte) (r, s *big.Int) {
+	return new(big.Int).SetBytes(src[:32]), new(big.Int).SetBytes(src[32:])
+}
+
+const ecdsaSignatureLength = 64
+
+func (s *Signature) Signature() (crypt.Signature, error) {
+	switch {
+	case s.Secp256k1 != nil:
+		if len(s.Secp256k1) != ecdsaSignatureLength {
+			return nil, errors.New("invalid Secp256k1 signature length")
+		}
+		r, s := point(s.Secp256k1)
+		return &crypt.ECDSASignature{
+			R:     r,
+			S:     s,
+			Curve: crypt.S256(),
+		}, nil
+
+	case s.P256 != nil:
+		if len(s.P256) != ecdsaSignatureLength {
+			return nil, errors.New("invalid P256 signature length")
+		}
+		r, s := point(s.P256)
+		return &crypt.ECDSASignature{
+			R:     r,
+			S:     s,
+			Curve: elliptic.P256(),
+		}, nil
+
+	case s.Ed25519 != nil:
+		if len(s.Ed25519) != ed25519.SignatureSize {
+			return nil, errors.New("invalid ed25519 signature length")
+		}
+		return crypt.Ed25519Signature(s.Ed25519[:]), nil
+
+	case s.BLS != nil:
+		sig, err := minpk.SignatureFromBytes(s.BLS[:])
+		if err != nil {
+			return nil, err
+		}
+		return (*crypt.BLSSignature)(sig), nil
+
+	default:
+		return nil, errors.New("malformed signature RPC response")
+	}
+}
+
+type RPCError struct {
+	Message string    `cbor:"message"`
+	Source  *RPCError `cbor:"source,omitempty"`
+}
+
+func (e *RPCError) Error() string {
+	return e.Message
+}
+
+func (e *RPCError) Unwrap() error {
+	if e.Source != nil {
+		return e.Source
+	}
+	return nil
+}
+
+type ImportResult struct {
+	PublicKey PublicKey `cbor:"public_key"`
+	Handle    uint64    `cbor:"handle"`
+}
+
+type GenerateResult struct {
+	EncryptedPrivateKey []byte    `cbor:"encrypted_private_key"`
+	PublicKey           PublicKey `cbor:"public_key"`
+}
+
+type GenerateAndImportResult struct {
+	EncryptedPrivateKey []byte    `cbor:"encrypted_private_key"`
+	PublicKey           PublicKey `cbor:"public_key"`
+	Handle              uint64    `cbor:"handle"`
+}
+
+type Result[T any] struct {
+	Ok  T         `json:",omitempty"`
+	Err *RPCError `json:",omitempty"`
+}
+
+func (r *Result[T]) Error() error {
+	if r.Err != nil {
+		return fmt.Errorf("RPC Error: %w", r.Err)
+	}
+	return nil
+}
