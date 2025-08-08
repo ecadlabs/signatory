2d536c953a6b950e4d95964dfc3aa67112db3a2d
Petel__ <petelauck@gmail.com>
2025-08-08 14:58:15 -0700

    Firestore watermark (#586)
    * rename watermark for dynamodb
    
    * add firestore backend for watermarking
    
    * fix using the same watermark for all request types
    
    * rename watermark struct for aws
    
    * rename watermark struct for gcp
    
    * extract gcp project id from credential file
    
    * add and update docs for gcp watermark backend
    
    * uncomment section for managing firestore collection in doc
    
    * update firestore configuration parameters in doc
    
    * remove section for managing firestore collection in doc
    
    * add dependency for gcp firestore
    
    * mod tidy
    
    * update GCP watermark registration to use GlobalContext instead of Config
    
    * add GCP configuration support to Cloud KMS backend
    
    * update gcp client initialization
    
    * update firestore watermark logic
    
    * rename field for project id for consistency
    
    * update firestore watermark doc

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.24
 toolchain go1.24.1
 
 require (
+	cloud.google.com/go/firestore v1.18.0
 	cloud.google.com/go/kms v1.22.0
 	github.com/aws/aws-sdk-go-v2 v1.37.2
 	github.com/aws/aws-sdk-go-v2/config v1.30.3
@@ -116,7 +117,7 @@ require (
 	golang.org/x/sys v0.35.0
 	golang.org/x/term v0.34.0
 	golang.org/x/text v0.28.0 // indirect
-	google.golang.org/grpc v1.74.2 // indirect
+	google.golang.org/grpc v1.74.2
 	google.golang.org/protobuf v1.36.7 // indirect
 )
 
---
+++ pkg/signatory/watermark/gcp.go
@@ -0,0 +1,133 @@
+package watermark
+
+import (
+	"context"
+	"fmt"
+
+	tz "github.com/ecadlabs/gotez/v2"
+	"github.com/ecadlabs/gotez/v2/crypt"
+	"github.com/ecadlabs/gotez/v2/protocol/core" // Import config directly
+	"github.com/ecadlabs/signatory/pkg/config"
+	"github.com/ecadlabs/signatory/pkg/signatory/request"
+	"github.com/ecadlabs/signatory/pkg/utils/gcp"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"gopkg.in/yaml.v3"
+
+	"cloud.google.com/go/firestore"
+)
+
+const (
+	defaultCollection = "watermark"
+)
+
+type GCPConfig struct {
+	gcp.Config `yaml:",inline"`
+	Project    string `yaml:"project" validate:"required"`
+	Database   string `yaml:"database" validate:"required"`
+	Collection string `yaml:"collection"`
+}
+
+func (c *GCPConfig) collection() string {
+	if c.Collection != "" {
+		return c.Collection
+	}
+	return defaultCollection
+}
+
+type GCP struct {
+	client *firestore.Client
+	col    *firestore.CollectionRef
+}
+
+func NewGCPWatermark(ctx context.Context, config *GCPConfig) (*GCP, error) {
+	var client *firestore.Client
+	var err error
+
+	opts, err := gcp.NewGCPOption(ctx, &config.Config)
+	if err != nil {
+		return nil, fmt.Errorf("(GCPWatermark) NewGCPWatermark: %w", err)
+	}
+
+	if config.Database == "" {
+		client, err = firestore.NewClient(ctx, config.Project, opts...)
+	} else {
+		client, err = firestore.NewClientWithDatabase(ctx, config.Project, config.Database, opts...)
+	}
+	if err != nil {
+		return nil, fmt.Errorf("(GCPWatermark) NewGCPWatermark: %w", err)
+	}
+
+	col := client.Collection(config.collection())
+
+	inst := GCP{
+		client: client,
+		col:    col,
+	}
+
+	return &inst, nil
+}
+
+type GCPWatermark struct {
+	Request string               `firestore:"request"`
+	Level   int32                `firestore:"lvl"`
+	Round   int32                `firestore:"round"`
+	Digest  *tz.BlockPayloadHash `firestore:"digest"`
+}
+
+func (f *GCP) IsSafeToSign(ctx context.Context, pkh crypt.PublicKeyHash, req core.SignRequest, digest *crypt.Digest) error {
+	m, ok := req.(request.WithWatermark)
+	if !ok {
+		// watermark is not required
+		return nil
+	}
+
+	docRef := f.col.Doc(m.GetChainID().String()).Collection(req.SignRequestKind()).Doc(pkh.String())
+
+	wm := request.NewWatermark(m, digest)
+
+	newWm := GCPWatermark{
+		Request: req.SignRequestKind(),
+		Level:   wm.Level,
+		Round:   wm.Round,
+		Digest:  wm.Hash.UnwrapPtr(),
+	}
+
+	return f.client.RunTransaction(ctx, func(ctx context.Context, tx *firestore.Transaction) error {
+		docSnap, err := tx.Get(docRef) // Read document
+
+		if err != nil {
+			if status.Code(err) == codes.NotFound {
+				// Document doesn't exist, safe to create
+				tx.Set(docRef, newWm)
+				return nil
+			}
+			return fmt.Errorf("(GCPWatermark) IsSafeToSign: %w", err)
+		}
+
+		// Document exists, check watermark
+		var oldWm GCPWatermark
+		if err := docSnap.DataTo(&oldWm); err != nil {
+			return fmt.Errorf("(GCPWatermark) IsSafeToSign: %w", err)
+		}
+
+		if oldWm.Level >= newWm.Level && (oldWm.Level != newWm.Level || oldWm.Round >= newWm.Round) {
+			return ErrWatermark
+		}
+
+		tx.Set(docRef, newWm)
+		return nil
+	})
+}
+
+func init() {
+	RegisterWatermark("gcp", func(ctx context.Context, node *yaml.Node, global config.GlobalContext) (Watermark, error) {
+		var config GCPConfig
+		if node != nil {
+			if err := node.Decode(&config); err != nil {
+				return nil, err
+			}
+		}
+		return NewGCPWatermark(ctx, &config)
+	})
+}
