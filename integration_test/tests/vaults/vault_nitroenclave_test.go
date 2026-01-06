ba759325b2e429ec754ee763a52dabb0f0db06ce
Petel__ <petelauck@gmail.com>
2026-01-06 01:08:28 -0800

    nitro integration test prints error for debug

--- integration_test/tests/vaults/vault_nitroenclave_test.go
+++ integration_test/tests/vaults/vault_nitroenclave_test.go
@@ -10,6 +10,9 @@ import (
 
 func TestNitroEnclaveVault(t *testing.T) {
 	out, err := integrationtest.OctezClient("-w", "1", "transfer", "1", "from", "nitro", "to", "alice", "--burn-cap", "0.06425")
+	if err != nil {
+		t.Logf("Error from OctezClient: %v, output: %s", err, string(out))
+	}
 	require.NoError(t, err)
 	require.Contains(t, string(out), "Operation successfully injected in the node")
 }
